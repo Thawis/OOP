3fb353d64127a921bda6ed8bf08c2c6e19b91ba8
Thawis <madokaqz@gmail.com>
2018-04-04 13:42:05 +0700

    add module_upload folder

--- gov_xml/test.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$doc = new DOMDocument();
-$doc->load ('899.xml');
-$public_date = $doc->getElementsByTagName("public_date_th")->item(0);
-echo $public_date;
-?>
--- gov_xml/test_create.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$myfile = fopen("newfile.txt","w") or die ("");
-$txt = "test1";
-fwrite($myfile,$txt);
-$txt = "test2";
-fwrite($myfile,$txt);
-fclose($myfile);
- ?>

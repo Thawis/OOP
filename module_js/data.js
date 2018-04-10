1ea1c5d9d7f0d8da4072b72308765171820c4544
Thawis <madokaqz@gmail.com>
2018-04-10 17:32:52 +0700

    add module_js

---
+++ module_js/data.js
@@ -0,0 +1,7 @@
+{
+  "verygood": 4,
+  "good": 5,
+  "normal": 6,
+  "bad": 1,
+  "verybad": 0,
+}
---
+++ module_js/update.php
@@ -0,0 +1,54 @@
+<?php
+
+// // Define the file here
+// define('JSON_FILE', 'data2.json');
+
+// // Create the empty file
+// if(!file_exists(JSON_FILE)) {
+//     $int_bytes = file_put_contents(JSON_FILE, json_encode((object)[
+//         'events' => ['First entry']
+//     ]));
+//     echo "Wrote {$int_bytes} bytes to new file", PHP_EOL;
+// }
+
+// // Load and decode
+// $obj_data = json_decode(file_get_contents(JSON_FILE));
+
+// // Show the data after loading
+// print_r($obj_data);
+
+// // // Set some data
+// $obj_data->awesome = true;
+// $obj_data->name = "tom";
+
+// // Add an event to the array
+// $obj_data->events[] = "Event at " . time();
+
+// // Show the data before saving
+// print_r($obj_data);
+
+// // Encode and save!
+// $int_bytes = file_put_contents(JSON_FILE, json_encode($obj_data));
+
+// echo "Wrote {$int_bytes} bytes", PHP_EOL;
+
+
+// foreach ($data as $key => $entry) {
+//     if ($entry['activity_code'] == '1') {
+//         $data[$key]['activity_name'] = "TENNIS";
+//     }
+// }
+
+//$jsonString = fill_get_contents('data2.json');
+$data = json_decode(file_get_contents('data3.json'));
+print_r($data);
+
+
+// Encode and save!
+// $newJsonString = json_encode($data);
+// file_put_contents('data2.json', $newJsonString);
+
+// $newJsonString = json_encode($data);
+// file_put_contents('jsonFile.json', $newJsonString);
+// print_r($data);
+?>

21a0c8d7d5258e27fe63e8b1454da110b5d8d54b
Thawis <t.thawee1994@gmail.com>
2018-05-10 18:18:34 +0700

    add dadnews

--- DAD/download_file.php
+++ DAD/download_file.php
@@ -1,7 +1,11 @@
 <?php 
+require_once 'config.php';
+require_once 'connection.php';
+
 // $name = 'ExampleFile.pdf';
-$name = 'u1.jpg';
-$path = 'Upload/doc/'.$name;
+// $name = 'u1.jpg';
+// $name = '20171027155047094_0001.jpg';
+// $path = 'Upload/doc/'.$name;
 
 // if(!$path){
 // 	die('file not found');
@@ -9,7 +13,7 @@ $path = 'Upload/doc/'.$name;
 // 	header("Cache-Control: public");
 // 	header("Content-Description: File Transfer");
 // 	header("Content-Disposition: attachment; filename=$name");
-// 	header("Content-Type: application/zip");
+// 	header("Content-Type: appli cation/zip");
 // 	header("Content-Transfer-Encoding: binary");
 //     //read the file from disk
 // 	readfile($file);
@@ -20,6 +24,19 @@ $path = 'Upload/doc/'.$name;
 // header("Content-disposition: attachment; filename=".$path);
 // readfile($path);
 
+
+// header('Content-Type: application/imgage');
+// header("Content-Transfer-Encoding: Binary");
+// header("Content-disposition: attachment; filename=".$path);
+// readfile($path);
+
+$sql = "SELECT * FROM news LEFT JOIN file ON news.ArticleID = file.ArticleID
+WHERE news.CategoryID = 4
+ORDER BY news.PublishDate DESC";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +45,14 @@ $path = 'Upload/doc/'.$name;
 	<title>TEST Download File</title>
 </head>
 <body>
-	<a href="<?=$path?>" download target="_blank">Click here to download</a>
+	<?php
+		while ($result = $stmt->fetch()) { 
+			$path = $result['File_Path'].$result['File_Name'];
+			$ID = $result['ArticleID'];
+			?>
+		<a href="<?=$path?>" download target="_blank">Click here to download <?=$ID?></a></br>
+	<?php	} ?>
+
+	<!-- <a href="<?=$path?>" download target="_blank">Click here to download</a> -->
 </body>
 </html>
---
+++ DAD_News/index.php
@@ -0,0 +1,132 @@
+<?php 
+session_start();
+require_once 'config.php';
+require_once 'connection.php'; 
+
+$sql = "SELECT * FROM news LEFT JOIN file ON news.ArticleID = file.ArticleID
+WHERE news.CategoryID = 4 AND Active = 1
+GROUP BY news.ArticleID
+ORDER BY news.PublishDate DESC";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$total_rows = $stmt->rowCount();
+
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1; 
+$perpage = 10;
+$record_num = ($perpage * $page) - $perpage;
+
+$sql = "SELECT * FROM news LEFT JOIN file ON news.ArticleID = file.ArticleID
+WHERE news.CategoryID = 4 AND Active = 1
+GROUP BY news.ArticleID
+ORDER BY news.PublishDate DESC LIMIT $record_num,$perpage";
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$num_record = $stmt->rowCount();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Search Page</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+    <?php include_once 'inc/head.php'; ?>
+
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <!-- ==================================================================== Search Result ==================================================================== -->
+                        <?php while($result = $stmt->fetch()){ ?>           
+                        <div class="hr-line-dashed"></div>
+                        <div class="search-result">
+                            <h3><a href="#"><?=$result['Title'] ?></a></h3>
+                            <!-- <a href="#" class="search-link">www.inspinia.com/inspinia</a> -->
+                            <p><?=$result['SubTitle'] ?></p>
+                        </div>
+                        <?php } ?>
+                        <!-- ==================================================================== Search Result ==================================================================== -->
+
+                        <?php 
+                        // $pages = isset($_GET['page']) ? intval($_GET['page']) : 1; 
+                        $total_pages = intval(ceil($total_rows / $perpage));
+                        if($num_record > 0 && $total_pages > 1){} 
+                            ?>
+                        <div class="hr-line-dashed"></div>
+                        <div class="text-center">
+                            <div class="btn-group">
+                                <?php if($page > 1){ ?>
+                                <a href="index.php?&page=1" class="btn btn-white"><i class="fa fa-step-backward"></i></a>
+                                <a href="index.php?&page=<?=$page-1?>" class="btn btn-white"><i class="fa fa-chevron-left"></i></a>
+                                <?php } ?>
+                                <?php 
+                                $range = 2;
+                                $initial_num = $page - $range;
+                                $condition_limit_num = ($page + $range)  + 1;
+
+                                for ($x=$initial_num; $x<$condition_limit_num; $x++) {
+                                    if (($x > 0) && ($x <= $total_pages)) {
+                                                                // current page
+                                        if ($x == $page) {
+                                            echo '<a class="btn btn-white active" href="index.php?&page='.$x.'">'.$x.'</a>';
+                                        } 
+                                                                // not current page
+                                        else {
+                                            echo '<a class="btn btn-white" href="index.php?&page='.$x.'">'.$x.'</a>';
+                                        }
+                                    }
+                                }
+                                ?>
+
+                                <?php if($page < $total_pages){ ?>
+                                <a href="index.php?&page=<?=$page+1?>" class="btn btn-white"><i class="fa fa-chevron-right"></i></a>
+                                <a href="index.php?&page=<?=$total_pages?>" class="btn btn-white"><i class="fa fa-step-forward"></i></a>
+                                <?php } ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="pull-right">
+            10GB of <strong>250GB</strong> Free.
+        </div>
+        <div>
+            <strong>Copyright</strong> Example Company &copy; 2014-2017
+        </div>
+    </div>
+
+</div>
+</div>
+
+
+<!-- Mainly scripts -->
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
+<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<!-- Custom and plugin javascript -->
+<script src="js/inspinia.js"></script>
+<script src="js/plugins/pace/pace.min.js"></script>
+
+
+</body>
+
+</html>
